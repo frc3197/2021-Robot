4009da3713d012543d1746d08841d03807e29191
YuriKleyman <kleymanyy@gmail.com>
2021-02-05 18:50:55 -0600

    Update Drive.java

--- 2021-Robot/src/main/java/frc/robot/commands/Drive.java
+++ 2021-Robot/src/main/java/frc/robot/commands/Drive.java
@@ -4,15 +4,13 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.SlewRateLimiter;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain.SwerveDrive;
 
 public class Drive extends CommandBase {
-
-  private final XboxController m_controller = new XboxController(0);
+  
   private final SwerveDrive m_swerve;
 
   private boolean fieldRelative = false;
@@ -43,18 +41,18 @@ public class Drive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //TODO: FIX EVERYTHING HERE OH GOD
+  
       // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
     final var xSpeed =
-        -m_xspeedLimiter.calculate(m_controller.getY(GenericHID.Hand.kLeft))
+        -m_xspeedLimiter.calculate(RobotContainer.getYLeft())
             * SwerveDrive.maxSpeed;
 
     // Get the y speed or sideways/strafe speed. We are inverting this because
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
     final var ySpeed =
-        -m_yspeedLimiter.calculate(m_controller.getX(GenericHID.Hand.kLeft))
+        -m_yspeedLimiter.calculate(RobotContainer.getXLeft())
             * SwerveDrive.maxSpeed;
 
     // Get the rate of angular rotation. We are inverting this because we want a
@@ -62,7 +60,7 @@ public class Drive extends CommandBase {
     // mathematics). Xbox controllers return positive values when you pull to
     // the right by default.
     final var rot =
-        -m_rotLimiter.calculate(m_controller.getX(GenericHID.Hand.kRight))
+        -m_rotLimiter.calculate(RobotContainer.getXRight())
             * SwerveDrive.maxSpeed;
 
     m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative);
